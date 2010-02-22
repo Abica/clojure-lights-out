3efd5ceabe9758f98431fc7684379a6b9b4d8f7e
Nicholas Wright <nick.loves.rails@gmail.com>
2010-02-22 01:32:07 -0700

    add leiningen project file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lights-out "1.0"
+  :description "The game of lights out written in clojure"
+  :url "http://github.com/Abica/clojure-lights-out"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :main lights)
